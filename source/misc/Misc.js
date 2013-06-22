5169e1cc9c01e3ac2812b9c7d5d2f5d0af4df8ea
Alex Tam Ming Yen <alex.tam@live.com>
2013-06-22 09:52:01 +0800

    Misc Character gen and counter finished

--- source/misc/Misc.js
+++ source/misc/Misc.js
@@ -21,9 +21,11 @@ enyo.kind({
 						{
 				        	classes:"centerDiv",
 				        	name:"pckType",
+				        	onChangeItem:"handlePickerChange",
 				            kind:"StandardPicker",
 				            datasource: [
-					            {content:"Character Count", type:0, active:true}					        
+					            {content:"Character Count", type:0, active:true},
+					            {content:"Character Generator", type:1}					        
 					        ]
 				        },
 				        { classes:"gap" },
@@ -41,7 +43,7 @@ enyo.kind({
 													kind:"onyx.TextArea",
 													name:"txtCode",
 													allowHtml:true,
-													placeholder:"Paste text here...",
+													placeholder:"Text goes here...",
 													style:"margin:15px;width:98%"
 												}
 											]
@@ -83,15 +85,35 @@ enyo.kind({
 			]
 		}
 	],
+	loremText:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	handlePickerChange:function(inSender,inEvent) {
+		
+	},
 	resetThis:function(inSender,inEvent) {
 		this.$.txtCharCount.setValue("");
 		this.$.txtCode.setValue("");
 	},
 	validateThis:function(inSender,inEvent) {
-		if(this.$.txtCode.getValue() != "" ){
-			this.$.txtCharCount.setValue( this.$.txtCode.getValue().length );
+		if (isNaN(this.$.txtCharCount.getValue())){
+			this.$.txtCharCount.setValue("");
+			this.phoneGap.alert("Text count is integers only");
 		} else {
-			this.phoneGap.alert("Please fill in the text to calculate.");
+			if (this.$.pckType.getSelected().type == 0){
+				if(this.$.txtCode.getValue() != "" ){
+					this.$.txtCharCount.setValue( this.$.txtCode.getValue().length );
+				} else {
+					this.phoneGap.alert("Please fill in the text to calculate.");
+				}
+			} else {
+				if(this.$.txtCharCount.getValue() != "" ){
+					this.$.txtCode.setValue( this.generateLorem(this.$.txtCharCount.getValue()) );
+				} else {
+					this.phoneGap.alert("Please fill in number of characters to generate.");
+				}
+			}
 		}
+	},
+	generateLorem:function(charCount){
+		return this.loremText.substring(0,charCount);
 	}
 });
